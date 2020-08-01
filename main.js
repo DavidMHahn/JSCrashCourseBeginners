161216ba31c5d0f4c70003f6c2b884a8d8bd0eec
DavidMHahn <davidmhahn@gmail.com>
2020-07-31 18:07:15 -0700

    Commit 07/31/2020

--- main.js
+++ main.js
@@ -53,20 +53,338 @@ console.log(s.split('')); //Split by letter
 const str = 'technology, computers, it, code';
 console.log(str.split(', ')); //Creates an array of the different values in the string.
 
-//23:50
+//Arrays - variables that hold mutiple values
+//old way using array constructor
+const numbers = new Array(1, 2, 3, 4, 5); //the 'new' key word is a constructor
 
+//New way is using brackets
+const fruits = ['apples', 'oranges', 'pears'];
 
+//You can have multiple data types within the same array
+const myArray = ['apples', 10, true, 'bananas'];
 
+//Accessing item(s) in array
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(fruits[1]); //Prints oranges since oranges is at index [1] position.
 
+//Adding items to an array. This method may not be good when you don't know
+//the length of your array.
+const fruits = ['apples', 'oranges', 'pears'];
+fruits[3] = 'grapes';
+console.log(fruits);
 
+//Using push method to add items to an array
+const fruits = ['apples', 'oranges', 'pears'];
+fruits.push('tomatoes');
+console.log(fruits);
 
+//Using unshift to add items to the beginning of an array
+const fruits = ['apples', 'oranges', 'pears'];
+fruits.unshift('bananas');
+console.log(fruits);
 
+//Using pop method to remove the last item off an array. Pop it off the end.
+const fruits = ['apples', 'oranges', 'pears'];
+fruits.pop();
+console.log(fruits);
 
+//To check if something is an array
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(Array.isArray(fruits)); //Prints true
 
+//To get the index of a certain value
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(fruits.indexOf('pears'));//Prints 2
 
+//Object literals - essentially key:value pairs
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 Main Street',
+        city: 'San Diego',
+        state: 'CA'
+    }
+}
+console.log(person); //Prints all values in the person object
+console.log(person.firstName);//Prints John
+console.log(person.firstName, person.lastName); //Prints John Doe
+console.log(person.hobbies[1]);//Prints movies
+console.log(person.address.city);//Prints San Diego
 
+//Destructuring
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 Main Street',
+        city: 'San Diego',
+        state: 'CA'
+    }
+}
+const { firstName, lastName } = person; //This isn't an assignment statement, but it is
+                                        //more like pulling out first and last name from
+                                        //the person object.
+console.log(firstName); //Prints John
 
+//If the item is embedded like the address values above, you would do the following:
+const { firstName, lastName, address: { city }} = person;
+console.log(city);//Prints San Diego
 
+//Adding properties to the object
+person.email = 'fred@barney.com';
+console.log(person); //Will show the object with an email key:value pair
 
+//Arrays of objects 
+//Ex: Array of todos
+const todos = [
+{
+    id: 1,
+    text: 'Take out trash',
+    isCompleted: true
+},
+{
+    id: 2,
+    text: 'Meet with Boss',
+    isCompleted: true
+},
+{
+    id: 3,
+    text: 'Dentist appointment',
+    isCompleted: false
+}
+];
+console.log(todos);
+console.log(todos[1].text);//Prints Meet with Boss
 
+//JSON data format.
+//after running our code through a free JSON formatter
+//(https://www.freeformatter.com/json-formatter.html)
+//We get the following output:
+[
+    {
+       "id": 1,
+       "text": "Take out trash",
+       "isCompleted": true
+    },
+    {
+       "id": 2,
+       "text": "Meet with Boss",
+       "isCompleted": true
+    },
+    {
+       "id": 3,
+       "text": "Dentist appointment",
+       "isCompleted": false
+    }
+ ]
+ //Note that the single quotes (' ') were replaced with double quotes (" ").
 
+//You can also convert the code into JSON format within the script
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meet with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    }
+    ];
+    const todoJSON = JSON.stringify(todos);
+    console.log(todoJSON);
+
+//For Loop
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//For Loop 2
+for(let i = 0; i <= 10; i++) {
+    console.log(`For Loop Number: ${i}`); //Using a template string using backticks ` `
+}
+
+//While Loop - in these loops you set the variable outside the loop
+let i = 0;
+while(i < 10) {
+    console.log(`While Loop Number: ${i}`);
+    i++;
+}
+
+//Looping through an Array using a for loop. Not an ideal way to loop through an array
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meet with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    }
+    ];
+    for(let i = 0; i < todos.length; i++) {
+        console.log(todos[i].text);
+    }
+
+//Looping through an array using for - of loop method
+    const todos = [
+        {
+            id: 1,
+            text: 'Take out trash',
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: 'Meet with Boss',
+            isCompleted: true
+        },
+        {
+            id: 3,
+            text: 'Dentist appointment',
+            isCompleted: false
+        }
+        ];
+        for(let todo of todos) { //todo is a variable, could be any name, todos is array
+            console.log(todo.text); //can use .text, .id, or no dot.
+        }
+
+//High order array methods - best way of iterating through arrays.
+//High order array methods take in a function as a parameter
+//forEach, map, filter
+
+//forEach (loops through the array items)
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meet with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    }
+    ];
+
+    todos.forEach(function(todo) {
+    console.log(todo.text);
+});
+
+//map - (allows us to create new array from existing array). map returns an array
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meet with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    }
+    ];
+
+    const todoText = todos.map(function(todo) { //We want to loop through array and
+                                                //return just the text values
+        return todo.text;
+
+});
+console.log(todoText);//Prints an array ["Take out trash", "Meet with Boss"...]
+
+//Filter (allows us to create a new array based on a condition)
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meet with Boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appointment',
+        isCompleted: false
+    }
+    ];
+
+    const todoComplete = todos.filter(function(todo) { //If we want to only return the 
+        return todo.isCompleted === true;              //todo items that are completed.
+}).map(function(todo) { //This allows us to get only the text from the completed todos
+    return todo.text;
+});
+console.log(todoComplete);
+
+//Conditionals
+const x = '10';
+if(x == 10) { // == doesn't care about data types, just the value. 10 == '10' is 'true'
+    console.log('x is 10');
+}
+
+const x = '10';
+if(x === 10) { // === data type matters, 10 === '10' is 'false'
+    console.log('x is 10');
+}
+else {
+    console.log('x is not 10'); //Prints this because '10' is a string not a number.
+}
+
+//else if
+const x = 11;
+if(x === 10) {
+    console.log('x is 10');
+}
+else if (x > 10) {
+    console.log('x is greater than 10');
+}
+else {
+    console.log('x is less than 10');
+}
+
+//Can do multiple conditions
+// or operator (||). One or the other conditions must be true
+const x = 4;
+const y =10;
+if(x > 5 || y > 10) {
+    console.log('x is more than 5 or y is more than 10');
+}
+
+// and operator (&&). Both conditions must be true.
+const x = 4;
+const y =10;
+if(x > 5 && y > 10) {
+    console.log('x is more than 5 and y is more than 10');
+}
+
+//Ternary operator
+51.23
